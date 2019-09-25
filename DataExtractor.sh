51256e33bfed993b5331b4c6a14a8a7e76e0bc6a
Supervitux <vitus.besel@web.de>
2019-09-25 15:34:46 +0300

    Added Header

--- DataExtractor.sh
+++ DataExtractor.sh
@@ -18,6 +18,8 @@ grep -E 'total E|full: ' Data1/logs/*.log | sed -n '1~2!p' | sed -n '1~3!p' > Da
 pathToLogs="./Data1/logs/"
 pathToCoords="./Data1/coord/"
 
+echo "LogPath,XYZPath,Dipole,Energy"  >> Data_Collection.csv
+
 while read p; do
    if [[ $p == *"total E"* ]]; then
        string2=`echo $p | awk '{print $4}'`
