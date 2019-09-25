e88f94a85055553b16cff5b683bc391710b03928
Supervitux <vitus.besel@web.de>
2019-09-25 14:55:15 +0300

    Relative paths

--- DataExtractor.sh
+++ DataExtractor.sh
@@ -3,15 +3,15 @@
 # The purpose of this script is to get the final total energy and dipoles of all .log files
 # We grep
 # This script fails if the .log files have a different structure than expected
-
+rm Data_Collection.csv
 # Greps all the lines with 'total E' and 'full', delete first every second and then every third line
 grep -E 'total E|full: ' Data1/logs/*.log | sed -n '1~2!p' | sed -n '1~3!p' > Data_Collection.temp
 
 # I still have to make warnings if the output file exists etc....
 #Too lazy now
-path=`pwd`
-pathToLogs=$path"/Data1/logs/"
-pathToCoords=$path"/Data1/coord/"
+#path=`pwd`
+pathToLogs="./Data1/logs/"
+pathToCoords="./Data1/coord/"
 
 while read p; do
    if [[ $p == *"total E"* ]]; then
