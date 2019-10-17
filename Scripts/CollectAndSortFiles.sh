2e5374080156901a1f4c968ffa42bb6899137128
Supervitux <vitus.besel@web.de>
2019-10-17 14:42:00 +0300

    Code depends now on paths as input

--- Scripts/CollectAndSortFiles.sh
+++ Scripts/CollectAndSortFiles.sh
@@ -1,7 +1,19 @@
 # Call this script from the folder with the configuration XTB folders
 
+
+name_dir=$1
+path_xtb=$2
+wrk_dir=`pwd`
+
+mkdir ../$name_dir
+mkdir ../$name_dir/logs
+mkdir ../$name_dir/coords
+
 a=0
 b=0
+
+cd $path_xtb
+
 for i in `ls -d XTB*`
         do echo $i
         cd $i
@@ -12,19 +24,12 @@ for i in `ls -d XTB*`
            name_xyz_it="$a"".xyz"   
            name_bet=${j%%.*}
            name_xyz="$name_bet"".xyz"
-#           echo $name_log " " $name_xyz_it " " $name_bet " " $name_xyz
-           cp $j ../../../logs/$name
-           cp $name_xyz ../../../coords/$name_xyz_it
-#           echo $name_log 
-#           echo $name_xyz
+           cp $j $wrk_dir/$1/logs/$name
+           cp $name_xyz $wrk_dir/$1/coords/$name_xyz_it
             let a=a+1
         done
-#       for k in `ls *.xyz`; 
-#           do echo $k
-#           name2="$b"".xyz"
-#           cp $k ../../../coords/$name2
-#           echo $name2
-#           let b=b+1
-#       done
+
         cd ../..
     done
+
+cd $wrk_dir
